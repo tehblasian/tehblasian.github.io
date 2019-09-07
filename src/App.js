3af06b3ec8b13dcce61266d4586fefb0237d74a4
tehblasian <jeremiahdavid.wreh@gmail.com>
2019-09-07 18:00:30 -0400

    Added full-height container component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './App.css';
 
 import Main from './pages/Main';
 
---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Container from '../components/Container';
+
+const Main = () => {
+    return (
+        <div>
+            <Container>
+                <h1>Jeremiah-David Wreh</h1>
+            </Container>
+        </div>
+    );
+};
+
+export default Main;
