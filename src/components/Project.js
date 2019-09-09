397fcc38c5542125dcaf7a8e78bbd24c9c148a2f
tehblasian <jeremiahdavid.wreh@gmail.com>
2019-09-08 22:06:25 -0400

    Fix project links alignment for mobile

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,15 +6,19 @@ import TagList from './TagList';
 
 const Project = ({ thumbnail, name, summary, tech, date, source, devpost = null }) => (
     <span style={{ display: 'flex', flexDirection: 'column', margin: '0.5em' }}>
-        <div style={{ display: 'flex', flexDirection: 'row'}}>
-            <Icon src={thumbnail} size="32px" alt={name} /> 
-            <h1 className="header-medium-1" style={{ margin: '0 0 0 0.5em' }}>{name}</h1>
-            <a href={source} target="_blank" rel="noopener noreferrer">
-                <h1 className="header-small" style={{ marginLeft: '1em' }}>source</h1>
-            </a>
-            {devpost && <a href={devpost} target="_blank" rel="noopener noreferrer">
-                <h1 className="header-small" style={{ marginLeft: '1em' }}>devpost</h1>
-            </a>}
+        <div className="project-header">
+            <div className="project-logo-name">
+                <Icon src={thumbnail} size="32px" alt={name} /> 
+                <h1 className="header-medium-1" style={{ margin: '0 0 0 0.5em' }}>{name}</h1>
+            </div>
+            <div className="project-links">
+                <a href={source} target="_blank" rel="noopener noreferrer">
+                    <h1 className="header-small" style={{ marginLeft: '1em' }}>source</h1>
+                </a>
+                {devpost && <a href={devpost} target="_blank" rel="noopener noreferrer">
+                    <h1 className="header-small" style={{ marginLeft: '1em' }}>devpost</h1>
+                </a>}
+            </div>
         </div>
         <h1 className="header-medium-2" style={{ margin: 0 }}>{summary}</h1>
         <h1 className="header-small">{date}</h1>
